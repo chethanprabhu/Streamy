ffb9f898f77c1ba7cfd6ca961e550790046c6669
Chethan Prabhu <chethanprabhu65@gmail.com>
2023-04-16 00:50:10 +0530

    .

--- src/pages/Trending/Trending.tsx
+++ src/pages/Trending/Trending.tsx
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
 const Trending = () => {
+
+    const [state, setState] = useState([])
+
+    const fetchTrending = async () => {
+        const {data} = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY_V3}`)
+        console.log(state)
+        setState(data.results);
+    }
+
+    useEffect(() => {
+        fetchTrending()
+    }, [])
+
     return (
         <div>
-            Trending
+           
         </div>
     )
 }
