d7e0222ee5be0b8ad84a0911be6766ec72e3446f
Chethan Prabhu <chethanprabhu65@gmail.com>
2023-04-16 23:35:05 +0530

    before npm run eject

--- src/pages/Trending/Trending.tsx
+++ src/pages/Trending/Trending.tsx
@@ -7,7 +7,7 @@ const Trending = () => {
 
     const fetchTrending = async () => {
         const {data} = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY_V3}`)
-        console.log(state)
+        console.log(data)
         setState(data.results);
     }
 
